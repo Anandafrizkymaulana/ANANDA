47b1affc84ff1eedf584d84d383f9d205031d25b
Anandafrizkymaulana <frizkymananda@gmail.com>
2019-12-05 20:57:19 +0700

    Ananda

---
+++ latihan3/src/Latihan3/percobaan1.java
@@ -0,0 +1,13 @@
+package Latihan3;
+
+/**
+ *
+ * @author Ananda Frizky Maulana
+ */
+public class percobaan1 {
+    
+    public static void main(String[] args) {
+        //Menampilkan kata "Hello world" dilayar
+        System.out.println("Hello world!");
+    }
+}
